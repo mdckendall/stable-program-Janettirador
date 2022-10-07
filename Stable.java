e63e0dbdeea671335a95e671ac9f153cf3da768c
JanetTirador <113356729+Janettirador@users.noreply.github.com>
2022-10-07 21:50:58 +0000

    Class Stable

---
+++ Stable.java
@@ -0,0 +1,15 @@
+
+class Stable {  
+    int address;
+    int horse;  
+ 
+    public static void main(String args[])
+    {  
+        Horse s1=new 
+Horse();  
+        System.out.println(s1.address);  
+        System.out.println(s1.horse); 
+    }
+}
+ 
+  
