32bf620d7f761c8c3250b9d58e3343783def0239
JanetTirador <113356729+Janettirador@users.noreply.github.com>
2022-10-07 19:55:01 +0000

    Added array list

--- Horse.java
+++
@@ -1,1 +0,0 @@
-//Create the Horse class here.
--- Stable.java
+++ Stable.java
@@ -1,1 +1,12 @@
-//Create the Stable class here.
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+    ArrayList<String> horse = new ArrayList<String>();
+    horse.add("Muffin");
+    horse.add("Red");
+    horse.add("Night");
+    horse.add("Luna");
+    System.out.println(horse);
+  }
+}
